234b9aba37f3d2693338fa4815ed12fba1680b49
Maurisca <maurizio.mml@gmail.com>
2023-08-07 11:39:08 -0300

    actividad 9

--- src/controllers/libroController.js
+++ src/controllers/libroController.js
@@ -1,5 +1,71 @@
+// const Libro = require("../models/libroModel");
+
+// exports.getAllLibros = async (req, res) => {
+//   try {
+//     const libros = await Libro.find();
+//     res.status(200).json(libros);
+//   } catch (error) {
+//     res.status(500).json({ error: "Error al obtener los libros" });
+//   }
+// };
+
+// exports.getLibroById = async (req, res) => {
+//   try {
+//     const libro = await Libro.findById(req.params.id);
+//     if (!libro) {
+//       return res.status(404).json({ error: "Libro no encontrado" });
+//     }
+//     res.status(200).json(libro);
+//   } catch (error) {
+//     res.status(500).json({ error: "Error al obtener el Libro" });
+//   }
+// };
+
+// exports.createLibro = async (req, res) => {
+//   try {
+//     const nuevoLibro = await Libro.create(req.body);
+//     await nuevoLibro.save();
+//     res.status(201).json(nuevoLibro);
+//   } catch (error) {
+//     res.status(500).json({ error: "Error al crear el Libro" });
+//   }
+// };
+
+// exports.updateLibro = async (req, res) => {
+//   try {
+//     const libro = await Libro.findByIdAndUpdate(req.params.id, req.body, {
+//       new: true,
+//     });
+
+//     if (!libro) {
+//       return res.status(404).json({ error: "Libro no encontrado" });
+//     }
+
+//     res.status(200).json(libro);
+//   } catch (error) {
+//     res.status(500).json({ error: "Error al actualizar el Libro" });
+//   }
+// };
+
+// exports.deleteLibro = async (req, res) => {
+//   try {
+//     const libroId = req.params.id;
+
+//     const libroEliminado = await Libro.findByIdAndRemove(libroId);
+
+//     res.status(200).json(libroEliminado);
+//   } catch (error) {
+//     res.status(500).json({ error: "Error al eliminar el Libro" });
+//   }
+// };
+
+
+
+// nueva parte, hecha con codeium
+// Importar el modelo de Libro
 const Libro = require("../models/libroModel");
 
+// Obtener la lista completa de libros
 exports.getAllLibros = async (req, res) => {
   try {
     const libros = await Libro.find();
@@ -9,6 +75,7 @@ exports.getAllLibros = async (req, res) => {
   }
 };
 
+// Obtener los detalles de un libro específico por su ID
 exports.getLibroById = async (req, res) => {
   try {
     const libro = await Libro.findById(req.params.id);
@@ -17,20 +84,22 @@ exports.getLibroById = async (req, res) => {
     }
     res.status(200).json(libro);
   } catch (error) {
-    res.status(500).json({ error: "Error al obtener el Libro" });
+    res.status(500).json({ error: "Error al obtener el libro" });
   }
 };
 
+// Crear un nuevo libro
 exports.createLibro = async (req, res) => {
   try {
     const nuevoLibro = await Libro.create(req.body);
     await nuevoLibro.save();
     res.status(201).json(nuevoLibro);
   } catch (error) {
-    res.status(500).json({ error: "Error al crear el Libro" });
+    res.status(500).json({ error: "Error al crear el libro" });
   }
 };
 
+// Actualizar la información de un libro específico por su ID
 exports.updateLibro = async (req, res) => {
   try {
     const libro = await Libro.findByIdAndUpdate(req.params.id, req.body, {
@@ -43,10 +112,11 @@ exports.updateLibro = async (req, res) => {
 
     res.status(200).json(libro);
   } catch (error) {
-    res.status(500).json({ error: "Error al actualizar el Libro" });
+    res.status(500).json({ error: "Error al actualizar el libro" });
   }
 };
 
+// Eliminar un libro específico por su ID
 exports.deleteLibro = async (req, res) => {
   try {
     const libroId = req.params.id;
@@ -55,6 +125,6 @@ exports.deleteLibro = async (req, res) => {
 
     res.status(200).json(libroEliminado);
   } catch (error) {
-    res.status(500).json({ error: "Error al eliminar el Libro" });
+    res.status(500).json({ error: "Error al eliminar el libro" });
   }
 };
